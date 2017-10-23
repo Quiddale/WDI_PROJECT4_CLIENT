f7604a61945a34acc0962a8c3bfe87aa62645cef
Quiddale <qosullivan962@gmail.com>
2017-10-23 15:34:43 +0100

    update

--- src/js/constants/api.constant.js
+++ src/js/constants/api.constant.js
@@ -1,3 +1,4 @@
 angular
 .module('pokemon')
-.constant('API', 'https://thawing-beach-79814.herokuapp.com/api');
+.constant('API', 'https://localhost:3000/API');
+// .constant('API', 'https://thawing-beach-79814.herokuapp.com/api');
